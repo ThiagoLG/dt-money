d1dc45dc1d29a9bc6cfcfc813d0d9424d5777eae
ThiagoLG <thiago.lg53@gmail.com>
2023-07-08 18:10:25 -0300

    feat: :tada: Setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  )
+}
