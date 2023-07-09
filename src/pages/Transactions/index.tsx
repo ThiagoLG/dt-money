f87f5b7a8ba5b368192677a7101405b2b6a718eb
ThiagoLG <thiago.lg53@gmail.com>
2023-07-08 21:50:01 -0300

    feat(header): :sparkles: Created transactions table on Transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,48 @@
 import * as React from 'react'
 import { Header } from '../../components/Header/index'
 import { Summary } from '../../components/Summary/index'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Site development</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Sell</td>
+              <td>07/08/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Dinner</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 90,00</PriceHighlight>
+              </td>
+              <td>Sell</td>
+              <td>07/08/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Site development</td>
+              <td>R$ 12.000,00</td>
+              <td>Sell</td>
+              <td>07/08/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
