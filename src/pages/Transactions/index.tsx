3f43e2de3dd33bba7ff74befed0c3a205766fde2
ThiagoLG <thiago.lg53@gmail.com>
2023-07-08 21:34:09 -0300

    feat(header): :sparkles: Created summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import { Header } from '../../components/Header/index'
+import { Summary } from '../../components/Summary/index'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
